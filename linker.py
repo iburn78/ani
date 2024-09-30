b55a6b7133a92b8cd48b631847256e1036d4bc12
Andy Kim <andy.chiwon.kim@gmail.com>
2024-09-30 19:33:28 +0900

    wip

--- linker.py
+++ linker.py
@@ -1,25 +1,29 @@
 #%% 
 from moviepy.editor import VideoFileClip, concatenate_videoclips
+from datetime import datetime
 
+# List of video file names
 videos = [
-    '2024-09-22 14-07-06.mp4', 
-    '2024-09-22 14-09-13.mp4',
-    '2024-09-22 14-10-53.mp4',
-    '2024-09-22 14-11-41.mp4',
-    '2024-09-22 14-13-24.mp4'
+    '2024-09-30 18-23-52.mp4', 
+    '2024-09-30 18-28-34.mp4',
+    '2024-09-30 18-33-31.mp4',
 ]
+
+# Update paths for video files
 for i, v in enumerate(videos):
-    videos[i] = 'videos/'+v
-# Load the video files
-video_A = VideoFileClip(videos[0]).subclip(0, 50)  # A: from start to 50 sec
-video_B = VideoFileClip(videos[1]).subclip(0, 73)  # B: from start to 13 sec
-video_C = VideoFileClip(videos[2]).subclip(0, 27)  # C: from start to 27 sec
-video_D = VideoFileClip(videos[3]).subclip(0, 90)  # D: from start to 1 min 30 sec
-video_E = VideoFileClip(videos[4])                 # E: from start to end
+    videos[i] = 'data/video/' + v
+
+# Load the video files with specified subclips
+video_A = VideoFileClip(videos[0]).subclip(0, VideoFileClip(videos[0]).duration)  # A: full video
+video_B = VideoFileClip(videos[1]).subclip(1, VideoFileClip(videos[1]).duration)  # B: from 1 sec to end
+video_C = VideoFileClip(videos[2]).subclip(0, VideoFileClip(videos[2]).duration)  # C: full video
 
 # Concatenate the clips
-final_video = concatenate_videoclips([video_A, video_B, video_C, video_D, video_E])
+final_video = concatenate_videoclips([video_A, video_B, video_C])
+
+# Increase the volume of the final concatenated video by a factor of 2 (double the volume)
+final_video_vol_ajusted = final_video.volumex(2)
 
 # Export the final video
-final_video.write_videofile("videos/final_video.mp4", codec="libx264")
+final_video_vol_ajusted.write_videofile("data/video/final_video.mp4".replace('.mp4', '_'+datetime.now().strftime('%Y-%m-%d-%H-%M')+'.mp4'), codec="libx264")
 
