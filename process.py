c4d9d8c931c07b339736405e432b85134f1d838d
Andy Kim <andy.chiwon.kim@gmail.com>
2024-10-11 15:38:02 +0900

    wip

--- process.py
+++ process.py
@@ -9,13 +9,15 @@ CONF_FILE = '../config/config.json'
 CLIENT_SECRETS_FILE = "../config/google_client.json"
 
 # ------------------------------------------------
-k_ppt_file = '.pptx'
-k_title = ''
+k_ppt_file = '삼성전자_2024_3Q_K_2024-10-11_shorts.pptx'
+k_title = '어닝쇼크 3분기 실적 반영하면, 삼성전자 지금 충분히 하락했다고 볼수 있을까?'
 k_desc = '''
+#삼성전자 #3분기실적 #PER
+3분기 잠정실적 반영하면, 역사적 PER와 비교해 봤을때 현재 PER는 어느정도 수준인지 살펴보겠습니다
 '''
-k_keywords = ['', '']
+k_keywords = ['SamsungElectronics', '3Q', 'PER']
 
-e_ppt_file = '.pptx'
+e_ppt_file = k_ppt_file.replace('_K_', '_E_')
 e_title, e_desc = translate_title_desc(k_title, k_desc, CONF_FILE)
 print(e_title)
 print(e_desc)
@@ -30,16 +32,24 @@ e_keywords = k_keywords
 #%% ----------------------------------------------
 # ✓ Generate Korean Video with Voice
 # ------------------------------------------------
-k_meta = Meta(ppt_file=k_ppt_file, image_prefix='슬라이드', google_application_credentials=GCA, lang='K')
+k_meta = Meta(ppt_file=k_ppt_file, image_prefix='슬라이드', google_application_credentials=GCA, lang='K',
+    fade_after_slide=[0, 1, 2, 4, 5],
+    )
 ppt_to_video(k_meta)
 
 #%% ----------------------------------------------
 # ✓ Translate to English and generate video
 # ------------------------------------------------
-e_ppt_file = k_ppt_file.replace('_K_', '_E_')
-e_meta = Meta(ppt_file=e_ppt_file, image_prefix='슬라이드', google_application_credentials=GCA, lang='E')
-
+e_meta = Meta(
+    ppt_file=e_ppt_file, image_prefix='슬라이드', google_application_credentials=GCA, lang='E', 
+    convert_slides_upto_slide_no=0, 
+    fade_after_slide=[0, 1, 2, 4, 5],
+    speaking_rate_EN=1.1, 
+    )
 num = gen_Eng_notes_from_Korean(e_meta, CONF_FILE)
+#%% 
+# if needed modify the script here.
+#%% 
 timepoints = ppt_tts(e_meta, num)
 video_from_ppt_and_voice(e_meta, timepoints)
 
