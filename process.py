c6a7094e0f2e3fe0b9890c3fbecf4c1a183fadf3
Andy Kim <andy.chiwon.kim@gmail.com>
2024-10-16 11:41:18 +0900

    wip

--- process.py
+++ process.py
@@ -9,13 +9,13 @@ CONF_FILE = '../config/config.json'
 CLIENT_SECRETS_FILE = "../config/google_client.json"
 
 # ------------------------------------------------
-k_ppt_file = '배당분석_K_2024-10-14.pptx'
-k_title = '배당이 증가하는 국내 상장사는? 매년 더 주는 회사 있나요?'
+k_ppt_file = '배당분석_K_2024-10-15.pptx'
+k_title = '국내 배당수익률 최고인 회사는? 지난 3년간 분석'
 k_desc = '''
-#배당 #매년증가 #투자전략
-해외에는 배당이 증가하는 종목을 골라서 투자하는 ETF도 있는데, 우리나라는 이런 전략이 통할지 살펴보겠습니다.
+#배당수익률 #배당 #투자전략
+2021, 2022, 2023년 배당수익률이 최고인 회사는 어디인지 알아보고, 이를 추종하는 전략이 유효할지 알아보겠습니다. 간단한 분석입니다.
 '''
-k_keywords = ['Dividend', 'EverIncreasing', 'Strategy']
+k_keywords = ['Dividend', 'Yield', 'Strategy']
 
 e_ppt_file = k_ppt_file.replace('_K_', '_E_')
 e_title, e_desc = translate_title_desc(k_title, k_desc, CONF_FILE)
