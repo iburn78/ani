5889858e49e723ee0437b43ca28c36f3b0f8d8d7
Andy Kim <andy.chiwon.kim@gmail.com>
2024-10-22 18:42:18 +0900

    wip

--- process.py
+++ process.py
@@ -9,13 +9,13 @@ CONF_FILE = '../config/config.json'
 CLIENT_SECRETS_FILE = "../config/google_client.json"
 
 # ------------------------------------------------
-k_ppt_file = 'SK하이닉스_K_2024-10-17.pptx'
-k_title = 'SK하이닉스, 삼성전자와 비교분석한다면 - 고평가 vs 저평가?' 
+k_ppt_file = '삼성바이오_K_2024-10-22_shorts.pptx'
+k_title = '수주 잭팟 터진 삼성바이오로직스, Value 점검해보면?'
 k_desc = '''
-#SK하이닉스 #삼성전자 #비교분석 #고평가 #저평가 #PER
-SK하이닉스와 삼성전자 단순비교는 어렵겠지만, 양사가 HBM이슈에 동시에 큰 영향을 받고 있으니 비교해 보았습니다. 고평가 일까요 저평가 일까요?
+#삼성바이오로직스 #수주잭팟 #PER #Valuation
+오늘 1.7조원 수주를 발표한 삼성바이오로직스 순항 중인데요, Value측면에서 한번 점검해 보겠습니다.
 '''
-k_keywords = ['SK Hynix', 'Samsung Electronics', 'Strategy']
+k_keywords = ['Samsung Biologics', 'PER', 'Valuation']
 
 e_ppt_file = k_ppt_file.replace('_K_', '_E_')
 e_title, e_desc = translate_title_desc(k_title, k_desc, CONF_FILE)
@@ -33,7 +33,7 @@ e_keywords = k_keywords
 # ✓ Generate Korean Video with Voice
 # ------------------------------------------------
 k_meta = Meta(ppt_file=k_ppt_file, image_prefix='슬라이드', google_application_credentials=GCA, lang='K',
-    # fade_after_slide=[0, 1, 2, 3, 4, 5, 6],
+    fade_after_slide=[0, 1, 3, 4, 5, 6],
     )
 ppt_to_video(k_meta)
 
@@ -43,8 +43,8 @@ ppt_to_video(k_meta)
 e_meta = Meta(
     ppt_file=e_ppt_file, image_prefix='슬라이드', google_application_credentials=GCA, lang='E', 
     convert_slides_upto_slide_no=0, 
-    fade_after_slide=[0, 1, 2, 4, 8, 10, 11, 13, 14, 15],
-    speaking_rate_EN=1.1, 
+    fade_after_slide=[0, 1, 3, 4, 5, 6],
+    speaking_rate_EN=1.15, 
     )
 num = gen_Eng_notes_from_Korean(e_meta, CONF_FILE)
 #%% 
@@ -56,13 +56,13 @@ video_from_ppt_and_voice(e_meta, timepoints)
 #%% ----------------------------------------------
 # ✓ Upload Korean 
 # ------------------------------------------------
-thumbnail_file = k_ppt_file.replace('.pptx', '.PNG')
-# thumbnail_file = None
+# thumbnail_file = k_ppt_file.replace('.pptx', '.PNG')
+thumbnail_file = None
 upload_video(k_meta, k_title, k_desc, k_keywords, thumbnail_file=thumbnail_file, client_secrets_file=CLIENT_SECRETS_FILE)
 
 #%% ----------------------------------------------
 # ✓ Upload English 
 # ------------------------------------------------
-thumbnail_file = e_ppt_file.replace('.pptx', '.PNG')
-# thumbnail_file = None
+# thumbnail_file = e_ppt_file.replace('.pptx', '.PNG')
+thumbnail_file = None
 upload_video(e_meta, e_title, e_desc, e_keywords, thumbnail_file=thumbnail_file, client_secrets_file=CLIENT_SECRETS_FILE)
