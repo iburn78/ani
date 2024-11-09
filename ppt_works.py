6ba88d390f3069560dc74226d8e75a33839f8da3
Andy Kim <andy.chiwon.kim@gmail.com>
2024-11-09 09:01:03 +0900

    wip

--- ppt_works.py
+++ ppt_works.py
@@ -1,46 +1,151 @@
 #%% 
 from pptx import Presentation
+import pandas as pd
+from PIL import Image
+import os
 
-# Load the existing presentation
-prs = Presentation("data/ppt/삼성바이오로직스_K_2024-11-01_shorts_13sec.pptx")
 
-# Loop through each slide
-for slide_number, slide in enumerate(prs.slides, start=0):
-    print(f"\nSlide {slide_number}")
-    print(len(slide.shapes))
-    for shape in slide.shapes: 
-        print(shape.name)
+def replace_shape_text(shape, new_text):
+    if not shape.has_text_frame:
+        raise Exception('Not a text shape')
+    for paragraph in shape.text_frame.paragraphs:
+        runs = paragraph.runs
+        if runs:
+            # Overwrite existing runs to preserve as much formatting as possible
+            runs[0].text = new_text
+            # Clear remaining runs if there are any extra
+            for i in range(1, len(runs)):
+                runs[i].text = ""
+        else:
+            # Fallback if no runs exist: add a new run with the new text
+            new_run = paragraph.add_run()
+            new_run.text = new_text
+        if shape.text == new_text: 
+            return 
+        else: 
+            raise Exception('Text replacement error')
+
+def replace_shape_image(shape, new_image_path): 
+    if hasattr(shape, "image"):
+        # Get the position and size of the current image
+        left = shape.left
+        top = shape.top
+        width = shape.width
+        height = shape.height
+                
+        # Replace with a new image
+        slide.shapes.add_picture(new_image_path, left, top, width=width, height=height)
+        # Remove the old image shape
+        slide.shapes._spTree.remove(shape._element)
+    else: 
+        raise Exception('Not an image shape')
+
+def read_content_db(excel_path):
+    # DB structure
+    sheet_name = 'datasheet'
+    preset_columns = ["name", "lang", "date", "suffix", "slide", "type", "title", "subtitle", "image_path", "image", "desc", "note"]
     
-    # Loop through placeholders on the slide
-    for placeholder in slide.placeholders:
-        print(f"  Placeholder Index: {placeholder.placeholder_format.idx}")
-        print(f"  Placeholder Type: {placeholder.placeholder_format.type}")
-        print(f"  Placeholder Text: '{placeholder.text}'")
+    if os.path.exists(excel_path):
+        df = pd.read_excel(excel_path, sheet_name=sheet_name) 
+    else:
+        df = pd.DataFrame(columns=preset_columns)
+        with pd.ExcelWriter(excel_path) as writer:
+            df.to_excel(writer, sheet_name=sheet_name, index=False)
+    return df
 
-data = {
-    "title": "Quarterly Report",
-    "content": "This quarter's performance exceeded expectations.",
-    "table_data": [["Category", "Value"], ["Revenue", "$1M"], ["Profit", "$200K"]]
+def list_slide_layouts(prs):
+    for idx, layout in enumerate(prs.slide_layouts):
+        print(f"Layout {idx}: {layout.name}")
+
+def get_slide_type(prs, type):
+    for idx, layout in enumerate(prs.slide_layouts):
+        if type in layout.name: 
+            return layout
+
+def print_placeholder_idx(prs): 
+    for slide_number, slide in enumerate(prs.slides, start=0):
+        print(f"Slide number: {slide_number}")
+        print('--------------')
+        for placeholder in slide.placeholders:
+            print(f"Placeholder Index: {placeholder.placeholder_format.idx}")
+            print(f"Placeholder Type: {placeholder.placeholder_format.type}")
+            if hasattr(placeholder, 'text'):
+                print(f"Placeholder Text: '{placeholder.text}'")
+
+def make_square_img(image_path, fill_color=(255, 255, 255, 0)):
+    with Image.open(image_path) as img:
+        width, height = img.size
+        new_size = max(width, height)
+        
+        # Create a new image with the square size and fill color (transparent by default)
+        new_img = Image.new("RGBA", (new_size, new_size), fill_color)
+        # Paste the original image at the center of the new image
+        new_img.paste(img, ((new_size - width) // 2, (new_size - height) // 2))
+        new_img.save(image_path)
+
+pptx_filename = '삼성전자_K_2024-11-08_shorts_13sec.pptx'
+
+working_dir = 'data/ppt/'
+blank_filename = 'blank.pptx'
+content_db_filename = 'content_db.xlsx'
+
+blank_path = os.path.join(working_dir, blank_filename)
+content_db_path = os.path.join(working_dir, content_db_filename)
+prs = Presentation(blank_path)
+content_db = read_content_db(content_db_path)
+content_db['date'] = content_db['date'].dt.strftime('%Y-%m-%d')
+fa = pptx_filename.replace('.pptx', '').split('_')
+target_db = content_db.loc[(content_db['name'] == fa[0])
+            & (content_db['lang'] == fa[1])
+            & (content_db['date'] == fa[2])
+            & (content_db['suffix'] == fa[3]+'_'+fa[4])]
+
+placeholder_idx_dict = {
+    'title': {
+        'date': 11, 
+        'title': 0,
+        'subtitle': None, 
+        'image': None, 
+        'desc': None, 
+    }, 
+    'image': {
+        'date': None, 
+        'title': 0, 
+        'subtitle': 10, 
+        'image': 11, 
+        'desc': 12, 
+    }, 
+    'bullet': {
+        'date': None, 
+        'title': 0, 
+        'subtitle': 10, 
+        'image': None, 
+        'desc': 12, 
+    }, 
+    'close': {
+        'date': None, 
+        'title': 0, 
+        'subtitle': 10, 
+        'image': None, 
+        'desc': None, 
+    }, 
 }
 
-# Update placeholders
-slide = prs.slides[1]
-# slide.placeholders[10].text = data["title"]
-# slide.shapes[1].text = data['content']
-shape = slide.shapes[4]
-left = shape.left
-top = shape.top
-width = shape.width
-height = shape.height
+for index, row in target_db.iterrows():
+    slide = prs.slides.add_slide(get_slide_type(prs, row['type']))
+    tdict = placeholder_idx_dict[row['type']]
+    for i in ['date', 'title', 'subtitle', 'desc']: 
+        if tdict[i] != None: 
+            tph = next((obj for obj in slide.placeholders if obj.placeholder_format.idx == tdict[i]), None)
+            tph.text = row[i]
 
-# Remove the existing image shape
-slide.shapes._spTree.remove(shape._element)
+    if tdict['image'] != None: 
+        img = os.path.join(row['image_path'], row['image'])
+        make_square_img(img)
+        tph = next((obj for obj in slide.placeholders if obj.placeholder_format.idx == tdict['image']), None)
+        tph.insert_picture(img)
 
-# Add the new image in the same position and size
-new_image_path = 'data/ppt/005380_fh_operat_01_E.png'
-slide.shapes.add_picture(new_image_path, left, top, width, height)
+        
+prs.save(os.path.join(working_dir, pptx_filename))
 
-# slide.shapes[2].text = data['content']
-# slide.placeholders[1].text = data["content"]
-prs.save('data/ppt/temp.pptx')
-# %%
+
--- process.py
+++ process.py
@@ -10,18 +10,20 @@ CLIENT_SECRETS_FILE = "../config/google_client.json"
 YOUTUBE_CONF = '../config/youtube_conf.json'
 
 # ------------------------------------------------
-k_ppt_file = '시장반응_K_2024-11-04_shorts'
+k_ppt_file = '트럼프당선후지수변화_K_2024-11-08_shorts'
 k_title = '13초컷 - 삼성바이오로직스 3분기 실적!'
-k_title = '오늘 금투세 폐지에 따른 시장 반응, 시총 규모별로 살펴보면 어떨까?'
+k_title = '트럼프 당선 직후 미국과 한국의 주식시장 방향 변화 분석, 기타 주요국은?'
 k_desc = '''
-#금투세 #분위기전환 #폐지
-금투세 폐지 소식이 전달된 오늘, 주식 시장 반응을 시총별로 살펴보았습니다. 침체된 분위기를 반전시킬 만한 중요 호재일지까지는 판단 어렵지만, 그래도 한번 살펴보겠습니다! 
+#트럼프 #주식시장 #영향
+트럼프 당선 직후 미국은 시장이 사상 최고가를 경신중이라는데, 우리나라와 비교하면 어떠한지, 그리고 일본, 홍콩, 영국등 주요국은 어떠했는지 살펴보겠습니다.
 '''
-k_keywords = ['Financial Inv TAX', 'TAX Abolition', 'Quaterly Performance']
+k_keywords = ['Trump', 'Stock Index', 'US vs Korea', 'Quaterly Performance']
 
 e_ppt_file = k_ppt_file.replace('_K_', '_E_')
 e_ppt_file = (e_ppt_file if e_ppt_file.endswith('.pptx') else e_ppt_file + '.pptx')
 e_title, e_desc = translate_title_desc(k_title, k_desc, CONF_FILE)
+# titles should not be too long
+#%%
 print(e_title)
 print(e_desc)
 
@@ -39,12 +41,12 @@ k_meta = Meta(ppt_file=k_ppt_file, google_application_credentials=GCA, lang='K',
     # slide_break=0.1, 
     # line_break=0.1,
     # speaking_rate_KR=1.3, 
-    fade_after_slide=[0, 2, 4], 
+    fade_after_slide=[0, 2, 4, 6], 
     # convert_slides_upto_slide_no=0, 
-    # target_slide_for_video = [5, ],
-    # video_file_path = ['ani.mp4', ], 
-    # video_height_scale = [0.50, ], 
-    # video_location = [(50, 260), ], # list of (x,y)
+    target_slide_for_video = [1, 3, 5],
+    video_file_path = ['index_us.mp4', 'index_other.mp4', 'index_KR.mp4' ], 
+    video_height_scale = [0.45, 0.45, 0.45], 
+    video_location = [(40, 260), (40, 260), (40, 260)], # list of (x,y)
     # video_interrupt = True, 
     )
 ppt_to_video(k_meta)
@@ -57,12 +59,12 @@ e_meta = Meta(
     # slide_break=0.1, 
     # line_break=0.1,
     # convert_slides_upto_slide_no=0, 
-    fade_after_slide=[0, 2, 4], 
+    fade_after_slide=[0, 2, 4, 6], 
     # speaking_rate_EN=1.3, 
-    # target_slide_for_video = [5, ],
-    # video_file_path = ['ani.mp4', ], 
-    # video_height_scale = [0.50, ], 
-    # video_location = [(50, 260), ], # list of (x,y)
+    target_slide_for_video = [1, 3, 5],
+    video_file_path = ['index_us.mp4', 'index_other.mp4', 'index_KR.mp4' ], 
+    video_height_scale = [0.45, 0.45, 0.45], 
+    video_location = [(40, 260), (40, 260), (40, 260)], # list of (x,y)
     # video_interrupt = True, 
     )
 num = gen_Eng_notes_from_Korean(e_meta, CONF_FILE)
@@ -88,6 +90,7 @@ upload_video(k_meta, k_title, k_desc, k_keywords, thumbnail_file=thumbnail_file,
 #%% ----------------------------------------------
 # ✓ Upload English 
 # ------------------------------------------------
+#%% 
 with open(YOUTUBE_CONF, 'r') as json_file:
     config = json.load(json_file)
 thumbnail_file = None
