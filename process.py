a585499894b43870b7c351c33de6bf1e3c2bfbaf
Andy Kim <andy.chiwon.kim@gmail.com>
2024-11-15 15:12:47 +0900

    wip

--- process.py
+++ process.py
@@ -10,23 +10,22 @@ CLIENT_SECRETS_FILE = "../config/google_client.json"
 YOUTUBE_CONF = '../config/youtube_conf.json'
 
 # ------------------------------------------------
-k_ppt_file = '3분기실적분석_K_2024-11-13_shorts'
-k_title = '13초컷 - 삼성바이오로직스 3분기 실적!'
-k_title = '2024년 3분기 상장사 실적 분석 - 오늘까지 발표된것 총정리!'
+k_ppt_file = '트리플약세_K_2024-11-15_shorts'
+# k_title = '13초컷 - 삼성바이오로직스 3분기 실적!'
+k_title = '한국은 현재 트리플 약세... 얼마나 지속될까?'
 k_desc = '''
-#2024 #3Q #Quarterly #Performance #총정리 #어닝쇼크 #어닝서프라이즈
-2024년 3분기 실적 발표 시즌입니다. 오늘까지 발표된 3Q 실적을 어닝서프라이즈 및 어닝쇼크, 그리고 섹터별로 분석해 보았습니다. 
+#트리플강세 #트리플약세 #주식 #채권 #환율 #Cycle #트럼프 #양극화
+트럼프 당선 이후, 미국은 트리플 강세를 보이고, 한국은 트리플 약세를 보이고 있습니다. 극단적인 양극화가 순식간에 발생해 버렸습니다. 한번 살펴보겠습니다.
 '''
-k_keywords = ['KOSPI', 'KOSDAQ', 'EarningShock', 'EarningSurprise', '2024', '3Q', 'Quaterly Performance']
+k_keywords = ['Triple strength', 'Triple weakness', 'Trump', 'Quaterly Performance']
 
 e_ppt_file = k_ppt_file.replace('_K_', '_E_')
 e_ppt_file = (e_ppt_file if e_ppt_file.endswith('.pptx') else e_ppt_file + '.pptx')
-#%% 
 e_title, e_desc = translate_title_desc(k_title, k_desc, CONF_FILE)
 # titles should not be too long
 print(e_title)
 print(e_desc)
-
+#%%
 # e_title = ''
 # e_desc = '''
 # '''
@@ -59,7 +58,7 @@ e_meta = Meta(
     # slide_break=0.1, 
     # line_break=0.1,
     # convert_slides_upto_slide_no=0, 
-    fade_after_slide=[0, 1, 2, 3, 4, 5, 6, 7], 
+    fade_after_slide=[0, 1, 2, 3, 4, 5, 6, 7],
     # speaking_rate_EN=1.3, 
     # target_slide_for_video = [1, 3, 5],
     # video_file_path = ['index_us.mp4', 'index_other.mp4', 'index_KR.mp4' ], 
@@ -91,12 +90,6 @@ upload_video(k_meta, k_title, k_desc, k_keywords, thumbnail_file=thumbnail_file,
 # ✓ Upload English 
 # ------------------------------------------------
 #%% 
-e_title = 'Q3 2024 Earnings Analysis of Listed Companies in Korea - Summary of Announced Results'
-e_desc = '''
-#2024 #3Q #Quarterly #Performance #Summary #EarningsShock #EarningsSurprise  
-It's the earnings season for Q3 2024, Korea. We analyzed the Q3 results announced so far, focusing on earnings surprises, earnings shocks, and sector performance.
-'''
-e_keywords = ['KOSPI', 'KOSDAQ', 'EarningShock', 'EarningSurprise', '2024', '3Q', 'Quaterly Performance']
 with open(YOUTUBE_CONF, 'r') as json_file:
     config = json.load(json_file)
 thumbnail_file = None
